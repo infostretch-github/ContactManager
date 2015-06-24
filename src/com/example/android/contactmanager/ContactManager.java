b131676f22ceafc4cc35144e1a21b0b179481b27
devtestvj <Vijay_Shyamasundar@ISC-2103s-MBP.home>
2015-06-24 13:48:50 -0400

    _f2

--- src/com/example/android/contactmanager/ContactManager.java
+++ src/com/example/android/contactmanager/ContactManager.java
@@ -34,7 +34,7 @@ import android.widget.SimpleCursorAdapter;
 public final class ContactManager extends Activity
 {
 
-    public static final String TAG = "ContactManager_f1";
+    public static final String TAG = "ContactManager_f2";
 
     private Button mAddAccountButton;
     private ListView mContactList;
